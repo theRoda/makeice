5ec269c328fbe8285a766bde8cbe28e8e6d3ce5e
theRoda <thisreallylongname@gmail.com>
2024-05-24 04:27:22 -0400

    improve caesar

--- ciphers/caesar.py
+++ ciphers/caesar.py
@@ -1,8 +1,9 @@
 import string
 
 def makeCaesar(plaintext, key):
-    alphabet = string.ascii_lowercase
-    shifted = alphabet[key:] + alphabet[:key]
-    table = str.maketrans(alphabet, shifted)
-    encoded = plaintext.lower().translate(table)
-    return(encoded)
+    if isinstance(key, int):
+        alphabet = string.ascii_lowercase
+        shifted = alphabet[key:] + alphabet[:key]
+        table = str.maketrans(alphabet, shifted)
+        encoded = plaintext.lower().translate(table)
+        return(encoded)
--- makeice.py
+++ makeice.py
@@ -2,7 +2,7 @@ import ciphers as c
 
 test = 'This is a plaintext string.'
 def main():
-    ice = c.makeCaesar(test, 13)
+    ice = c.makeCaesar(test, 25)
     print(ice)
 
 if __name__ == "__main__":
